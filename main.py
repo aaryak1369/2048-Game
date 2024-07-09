161d96a378d8cc3a28adf1f36a4ef71206d182ec
aaryak4554 <joinaarya30@gmail.com>
2024-07-09 23:59:20 +0530

    added draw functions to render game board and tiles.

--- main.py
+++ main.py
@@ -51,7 +51,46 @@ class Tile:
         self.x = col * RECT_WIDTH
         self.y = row * RECT_HEIGHT
 
+    def get_color(self):
+        color_index = int(math.log2(self.value)) - 1
+        color = self.COLORS[color_index]
+        return color
 
+    def draw(self, window):
+        color = self.get_color()
+        pygame.draw.rect(window, color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
+
+        text = FONT.render(str(self.value), 1, FONT_COLOR)
+        window.blit(
+            text,
+            (
+                self.x + (RECT_WIDTH / 2 - text.get_width() / 2),
+                self.y + (RECT_HEIGHT / 2 - text.get_height() / 2),
+            ),
+        )
+
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+
+def draw(window, tiles):
+    window.fill(BACKGROUND_COLOR)
+
+    for tile in tiles.values():
+        tile.draw(window)
+
+    draw_grid(window)
+
+    pygame.display.update()
+    
 def get_random_pos(tiles):
     row = None
     col = None
