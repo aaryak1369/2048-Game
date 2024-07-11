e9df56e7c682978c25c7eef1cf72d0c429ffa4b9
aaryak4554 <joinaarya30@gmail.com>
2024-07-11 13:27:01 +0530

    Updated constants in main.py

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ COLS = 4
 RECT_WIDTH = WIDTH // COLS
 RECT_HEIGHT = HEIGHT // ROWS
 
-OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_COLOR = (253, 255, 226)
 OUTLINE_THICKNESS = 10
-BG_COLOR = (205,192,180)
-FONT_COLOR = (119, 110, 101)
+BG_COLOR = (131, 180, 255)
+FONT_COLOR =(90, 114, 160)
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -33,15 +33,15 @@ MOVE_VELOCITY = 20
 
 class Tile:
     COLORS = [
-        (237, 229, 218),
-        (238, 225, 201),
-        (243, 178, 122),
-        (246, 150, 101),
-        (247, 124, 95),
-        (247, 95, 59),
-        (237, 208, 115),
-        (237, 204, 99),
-        (236, 202, 80),
+        (243, 254, 184),
+        (255, 222, 77),
+       (255, 178, 44),
+       (255, 76, 76),
+        (192, 139, 92),
+        (121, 84, 88),
+       (69, 63, 120),
+      (69, 63, 120),
+     (69, 63, 120),
     ]
 
     def __init__(self, value, row, col):
